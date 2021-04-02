ffe736e7d888bdd612e538fc9d3bcb04cb8455e8
Ned Palacios <npdoesmc@gmail.com>
2021-04-02 21:09:31 +0800

    improve program

--- valgrind_test/open_close_doc.v
+++ valgrind_test/open_close_doc.v
@@ -14,25 +14,38 @@ fn main() {
 		root_uri: lsp.document_uri_from_path(testing.test_files_dir)
 	} ))
 
-	for test_name in ['completion', 'hover', 'workspace_symbols'] {
-		test_files := testing.load_test_file_paths(test_name) or { []string{} }
-		mut opened_docs := []lsp.TextDocumentIdentifier{len: test_files.len}
+	// for _ in 0 .. 5 {
+		// for test_name in ['completion', 'hover', 'workspace_symbols'] {
+			test_files := testing.load_test_file_paths('hover') or { []string{} }
+			mut opened_docs := []lsp.TextDocumentIdentifier{len: test_files.len}
+			mut sources := []string{len: test_files.len}
 
-		// open documents
-		for test_file_path in test_files {
-			content := os.read_file(test_file_path) or {
-				io.bench.fail()
-				eprintln(io.bench.step_message_fail('file $test_file_path is missing'))
-				continue
+			// open documents
+			for test_file_path in test_files {
+				content := os.read_file(test_file_path) or { continue }
+				_, doc_id := io.open_document(test_file_path, content)
+				sources << content
+				opened_docs << doc_id
 			}
 
-			_, doc_id := io.open_document(test_file_path, content)
-			opened_docs << doc_id
-			time.sleep(2 * time.second)
-		}
+			// close document
+			for idx, doc_id in opened_docs {
+				// edit document
+				for i in 0 .. 8 {
+					ls.dispatch(io.request_with_params('textDocument/didChange', lsp.DidChangeTextDocumentParams{
+						text_document: lsp.VersionedTextDocumentIdentifier{
+							uri: doc_id.uri
+							version: i
+						}
+						content_changes: [lsp.TextDocumentContentChangeEvent{
+							text: sources[idx]
+						}]
+					}))
+				}
 
-		for doc_id in opened_docs {
-			ls.dispatch(io.close_document(doc_id))
-		}
-	}
+				time.sleep(2 * time.second)
+				ls.dispatch(io.close_document(doc_id))
+			}
+		// }
+	// }
 }
